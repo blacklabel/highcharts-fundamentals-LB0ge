5b55a82ba7f5eaef992865b8c3a8bcc9dabaf68d
LB0ge <lars.boge@gmail.com>
2023-03-03 16:57:52 +0100

    created chart with connecting lines between column edges

---
+++ highcharts-api/highcharts/7-connecting-column-edges/main.js
@@ -0,0 +1,57 @@
+Highcharts.chart('container', {
+
+  chart: {
+    type: 'column',
+    events: {
+      load() {
+        const chart = this;
+        chart.customLines;
+      },
+      render() {
+        const chart = this,
+          yAxis = chart.yAxis[0],
+          allSeries = chart.series;
+
+        if (chart.customLines) {
+          chart.customLines.destroy();
+        }
+        chart.customLines = chart.renderer.g().add().toFront();
+
+        allSeries.forEach((series) => { // iterate through each series
+          let points = series.points;
+          if (series.visible) {
+            for (let i = 0; i < points.length - 1; i++) { // iterate through points
+              const xFrom = points[i].shapeArgs.x + chart.plotLeft,
+                xTo = points[i + 1].shapeArgs.x + chart.plotLeft,
+                yFrom = yAxis.toPixels(points[i].y),
+                yTo = yAxis.toPixels(points[i + 1].y),
+                colWidth = points[i].shapeArgs.width;
+
+              chart.renderer // draw line from current to next point
+                .path(['M', xFrom + colWidth - 1, yFrom + 2, 
+                  'L', xTo + 1, yTo + 2])
+                .attr({ 'stroke': series.color, 'stroke-width': '2px' })
+                .add(chart.customLines);
+            }
+          }
+        });
+      }
+    }
+  },
+
+  plotOptions: {
+    series: {
+      events: {
+        legendItemClick() {
+          const chart = this;
+          chart.redraw();
+        }
+      }
+    }
+  },
+
+  series: [...Array(3)].map(() => ({
+    data: [...Array(5)].map(() => Math.floor(Math.random() * 15) + 1)
+  }))
+
+});
